c3d3496970efd93c3fba23dae328f2e759480b1a
Laurence <thedailyhobbit@gmail.com>
2026-01-01 15:21:19 -0800

    "woo"

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from constants import *
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting Asteroids with pygame version:{pygame.version.vernum}")
-    print(f"Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    pygame.display.flip()
 
-    if __name__ == "__main__":
-        main()
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+if __name__ == "__main__":
+    main()
