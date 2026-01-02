0eb1d75890a1ce2674d5cd3d1ff2bddbb1bc0519
Laurence <thedailyhobbit@gmail.com>
2026-01-01 19:05:18 -0800

    "woo"

--- main.py
+++ main.py
@@ -4,20 +4,31 @@ from logger import log_state
 from player import *
 
 def main():
+    print("Entered main")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Created screen")
     clock = pygame.time.Clock()
+
+
     dt = 0
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT /2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+
     while True:
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
