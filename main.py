1018bf3f6ee4945e6371eb4a63d2bbb6ed8099d3
Laurence <thedailyhobbit@gmail.com>
2026-01-01 17:49:29 -0800

    "wasd"

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import *
 from logger import log_state
-
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT /2)
     while True:
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
 if __name__ == "__main__":
     main()
