c1cedef6d35d6e8de1524742f1c3049ae085a723
Laurence <thedailyhobbit@gmail.com>
2026-01-02 00:19:32 -0800

    ":3"

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import pygame
 from asteroid import *
 from asteroidfield import *
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
 from player import *
+from shot import *
+import sys
 
 def main():
     print("Entered main")
@@ -21,6 +23,9 @@ def main():
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
+
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
@@ -32,11 +37,24 @@ def main():
                 return
 
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
 
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) /  1000
+
 if __name__ == "__main__":
     main()
